d03fbd01ed137f55ef6ccfff1fe59be6ea0e78ff
Nodi Meladze <noe.meladze23@gmail.com>
2025-02-18 16:08:39 +0400

    migrations

--- PeopleDirectory.Infrastructure/Migrations/20250218120414_changes.Designer.cs
+++ PeopleDirectory.Infrastructure/Migrations/20250218120414_changes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PeopleDirectory.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using PeopleDirectory.Infrastructure.Data;
 namespace PeopleDirectory.Infrastructure.Migrations
 {
     [DbContext(typeof(PeopleDirectoryDbContext))]
-    partial class PeopleDirectoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250218120414_changes")]
+    partial class changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PeopleDirectory.Infrastructure/Migrations/20250218120414_changes.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeopleDirectory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
